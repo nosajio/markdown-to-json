ef78eca91310e045df40b6b2fdb55f1fdff9b94e
Jason Howmans <jason@nosaj.io>
2019-04-01 13:47:45 +0100

    Don't capture brackets in URL regexp group

--- parse/parse.go
+++ parse/parse.go
@@ -91,7 +91,7 @@ func sortFilesChronological(f []mdFile) ([]mdFile, error) {
 
 func parseBodyHTML(b []byte) []byte {
 	// Custom img tag
-	imgTagPattern := regexp.MustCompile(`(?im)\%img(\[.*\])(\(.*\))`)
+	imgTagPattern := regexp.MustCompile(`(?im)\%img\[(.*)\]\((.*)\)`)
 	b = imgTagPattern.ReplaceAll(b, []byte("<img src=\"$2\" alt=\"$1\" />"))
 	// Render standard markdown
 	bodyHTML := blackfriday.Run(b)
